0443ed2b042e67bdf2f3f487accf0609720cec3f
sharad15 <sharad@techalchemy.co>
2023-01-31 13:58:54 +0530

    href

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   <div className="mx-auto">
     <div className="flex items-center justify-between  py-2 md:justify-start md:space-x-10">
       <div className="flex justify-start lg:w-0 lg:flex-1 px-6">
-        <a href="#">
+        <a href="/">
           <span className="sr-only">MBD Financials</span>
           <img className="h-[85px] w-auto sm:h-[85px]" src={logo} alt="MBD Financials" />
         </a>
@@ -20,7 +20,7 @@ function App() {
         
       </nav>
       <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-        <a href="#" className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 mx-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Get Started</a>
+        <a href="/" className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 mx-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Get Started</a>
       </div>
     </div>
   </div>
